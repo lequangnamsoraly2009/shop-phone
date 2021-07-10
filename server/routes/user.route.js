99e8c0a0aea6777bc4682dfac1a13264809b560e
soraly <lequangnam1617@gmail.com>
2021-07-10 15:41:30 +0700

    login

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/user.controller')
 
 router.post('/register', userController.register)
 router.post('/refresh_token', userController.refreshToken)
+router.post('/login', userController.login)
 
 
 module.exports = router;
