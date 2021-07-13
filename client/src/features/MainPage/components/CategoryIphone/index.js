adb580d1bf7f971d5c74a43849cb36ca9d16fa47
soraly <lequangnam1617@gmail.com>
2021-07-13 23:18:18 +0700

    test 🦹‍♀️

---
+++ client/src/features/MainPage/components/CategoryIphone/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+function CategoryIphone() {
+  return (
+    <section className="soraly-section-2">
+      <Row gutter={[16,16]}>
+        <Col span={8}>
+          Col
+        </Col>
+      </Row>
+    </section>
+  );
+}
+
+export default CategoryIphone;
