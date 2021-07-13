f7d6d51d23bed8177c55540769a3d0cd9ec11810
soraly <lequangnam1617@gmail.com>
2021-07-14 00:08:05 +0700

    get category

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,6 +1,13 @@
+const Category = require('../models/category.model')
+
 const categoryController = {
     getCategories: async(req,res) => {
-        res.json("hi")
+        try {
+            const categories = await Category.find();
+            res.json(categories)
+        } catch (error) {
+            return res.status(500).json({status: false, message: error.message})
+        }
     }
 
 
