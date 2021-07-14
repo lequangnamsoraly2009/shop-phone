bf8fb855dd54e0e74bfd6a439860ab47e911ab03
soraly <lequangnam1617@gmail.com>
2021-07-14 21:57:05 +0700

    Add redux persist

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,41 @@
-import {configureStore } from '@reduxjs/toolkit'
-import tokenReducer from "./tokenSlice"
-import userReducer from './userSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import tokenReducer from "./tokenSlice";
+import userReducer from "./userSlice";
 
+const rootReducer = combineReducers({
+  token: tokenReducer,
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-        token: tokenReducer,
-        user : userReducer,
-    },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import {Provider} from 'react-redux'
+import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import {persistor} from "./app/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
