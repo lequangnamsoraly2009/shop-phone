442c192b390957490c02ba624d3e5f946c9bdaa0
soraly <lequangnam1617@gmail.com>
2021-07-15 04:57:14 +0700

    admin create category

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -8,6 +8,24 @@ const categoryController = {
         } catch (error) {
             return res.status(500).json({status: false, message: error.message})
         }
+    },
+    // Only Admin create, update, delete category 
+    createCategory: async(req, res) => {
+        try {
+            const {nameCategory} = req.body;
+            const category = await Category.findOne({nameCategory});
+            if(category) return res.status(400).json({status: false, message:"Category already exists"});
+
+            // New Category
+
+            const newCategory = new Category({nameCategory});
+
+            await newCategory.save();
+            res.json("Create success a category")
+            
+        } catch (error) {
+            return res.status(500).json({status: false, message: error.message})
+        }
     }
 
 
--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const categoryController = require("../controllers/category.controller")
+const auth = require("../middleware/auth")
+const authAdmin = require("../middleware/authAdmin")
 
 
-router.route("/category").get(categoryController.getCategories)
 
+router.route("/category").get(categoryController.getCategories);
+router.route("/admin/category").post(auth,authAdmin,categoryController.createCategory);
 
 module.exports = router;
