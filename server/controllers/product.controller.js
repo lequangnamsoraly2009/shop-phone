6d25558d7674ba4060b87e968dcf38ff6ebf1e2b
soraly <lequangnam1617@gmail.com>
2021-07-16 20:35:29 +0700

    get products

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -3,7 +3,8 @@ const Products = require("../models/product.model")
 const productController = {
     getProducts: async (req, res) => {
         try {
-            
+            const products  = await Products.find();
+            res.json(products)
         } catch (error) {
             return res.status(500).json({ status: false, message: error.message })
         }
