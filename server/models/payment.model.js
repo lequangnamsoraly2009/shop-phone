38d4f14bb06294ca2042334a7982da40dca4b639
soraly <lequangnam1617@gmail.com>
2021-07-21 13:15:10 +0700

    create modal payment

---
+++ server/models/payment.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    cart: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+    status: {
+      type: Boolean,
+      required: true,
+    },
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Payments", paymentSchema);
