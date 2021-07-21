b1d4a75134dde432fe7c8e1d61f562f64690b295
soraly <lequangnam1617@gmail.com>
2021-07-21 13:35:12 +0700

    payment controller

---
+++ server/controllers/payment.controller.js
@@ -0,0 +1,41 @@
+const Payments = require("../models/payment.model");
+const Users = require("../models/users.model");
+const Products = require("../models/product.model");
+
+const paymentController = {
+  getPayments: async (req, res) => {
+    try {
+      const payments = await Payments.find();
+      res.json(payments);
+    } catch (error) {
+      return res.status(500).json({ status: false, message: error.message });
+    }
+  },
+  createPayment: async (req, res) => {
+    try {
+      const user = await Users.findById(req.user.id);
+      if (!user)
+        return res
+          .status(400)
+          .json({ status: false, message: "User does not exist !" });
+
+      const { cart, paymentID, address } = req.body;
+      const { _id, name, email } = user;
+
+      const newPayment = new Payments({
+        user_id: _id,
+        name,
+        email,
+        cart,
+        paymenID,
+        address,
+      });
+
+      res.json({ newPayment });
+    } catch (error) {
+      return res.status(500).json({ status: false, message: error.message });
+    }
+  },
+};
+
+module.exports = paymentController;
--- server/models/payment.model.js
+++ server/models/payment.model.js
@@ -14,6 +14,10 @@ const paymentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    paymentID: {
+      type: String,
+      required: true,
+    },
     phone: {
       type: Number,
       required: true,
