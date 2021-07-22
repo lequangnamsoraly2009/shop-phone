76afd93c67a7e33c67a6dc8fe6e11465bb86008b
soraly <lequangnam1617@gmail.com>
2021-07-22 22:26:18 +0700

    history order

--- client/src/features/Customer/pages/HistoryCustomer/index.js
+++ client/src/features/Customer/pages/HistoryCustomer/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Breadcrumb, Table, Tooltip } from "antd";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./history.css";
 
 function HistoryCustomer() {
   const { history } = useSelector((state) => state.histories);
+  console.log(history);
 
   const columns = [
     {
-      title: "PaymentID",
+      title: "Code Orders",
       dataIndex: "paymentID",
       key: "paymentID",
       render: (text, record, index) => (
-        <Link  to={`/${record._id}`} style={{ fontSize: 12}}>{record.paymentID}</Link>
+        <Link to={`/customer/history/${record._id}`} style={{ fontSize: 12 }}>
+          {record.paymentID}
+        </Link>
       ),
       width: 300,
     },
@@ -36,13 +39,32 @@ function HistoryCustomer() {
         showTitle: false,
       },
       render: (text, record, index) => (
-        <Tooltip
-          placement="topLeft"
-          style={{ fontSize: 12 }}
-          title={record.cart[0].title}
-        >
-          <span style={{ fontSize: 12 }}>{record.cart[0].title}</span>
-        </Tooltip>
+        <>
+          {record.cart.length === 1 ? (
+            <Tooltip
+              placement="topLeft"
+              style={{ fontSize: 12 }}
+              title={record.cart[0].title}
+            >
+              <span style={{ fontSize: 12, textTransform: "capitalize" }}>
+                {record.cart[0].title}
+              </span>
+            </Tooltip>
+          ) : (
+            <Tooltip
+              placement="topLeft"
+              style={{ fontSize: 12 }}
+              title={`${record.cart[0].title} and ${
+                record.cart.length - 1
+              } other products`}
+            >
+              <span style={{ fontSize: 12, textTransform: "capitalize" }}>
+                {record.cart[0].title} and {record.cart.length - 1} other
+                products
+              </span>
+            </Tooltip>
+          )}
+        </>
       ),
     },
     {
@@ -50,7 +72,15 @@ function HistoryCustomer() {
       dataIndex: "price",
       key: "price",
       render: (text, record, index) => (
-        <span style={{ fontSize: 12 }}>{record.phone}</span>
+        <>
+          {
+            <span style={{ fontSize: 12 }}>
+              {record.cart.reduce((item1, item2) => {
+                return item1 + item2.price * item2.quantity;
+              }, 0)}
+            </span>
+          }
+        </>
       ),
       width: 140,
     },
@@ -58,7 +88,7 @@ function HistoryCustomer() {
       title: "Status",
       dataIndex: "status",
       key: "status",
-      render: (text) => <span style={{ fontSize: 12 }}>Success</span>,
+      render: (text) => <span style={{ fontSize: 12, color: "green" }}>Success</span>,
       width: 140,
     },
   ];
@@ -82,7 +112,7 @@ function HistoryCustomer() {
         <h2>Your History Order</h2>
       </div>
       <div className="history-table">
-        <Table columns={columns} dataSource={history} />  
+        <Table columns={columns} dataSource={history} />
         {/* Theem key vao dataSource */}
       </div>
     </div>
