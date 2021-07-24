567df386de35d777cc6d14ee590d024b1c75c100
soraly <lequangnam1617@gmail.com>
2021-07-25 00:27:24 +0700

    grid for category

--- client/src/features/Category/pages/categoryMain/index.js
+++ client/src/features/Category/pages/categoryMain/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Col, Row } from "antd";
+import React from "react";
 
 function Category() {
-    return (
-        <div className="container-fluid">
-            Hi Dashboard Category
-        </div>
-    )
+  return (
+    <div className="container-fluid">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col className="gutter-row" span={4}>
+          <div>col-4</div>
+        </Col>
+        <Col className="gutter-row" span={18}>
+          <div style={{border: "1px solid #000"}}>col-18</div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Category
+export default Category;
