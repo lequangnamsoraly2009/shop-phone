9067fae812dc6ebea80fd08a967a5f1f52bd933b
soraly <lequangnam1617@gmail.com>
2021-07-25 23:01:50 +0700

    fix product filter

--- client/src/api/filterAPI.js
+++ client/src/api/filterAPI.js
@@ -1,20 +1,21 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import API from "../api/axiosClient";
-import { getProducts } from "../app/productSlice";
+import { getProductsFilter, setResultFilter } from "../app/filterSlice";
 
-const ProductAPI = () => {
-  const {category,sort,search,page,result} = useSelector(state => state.products);
+const ProductFilterAPI = () => {
+  const {category,sort,search,page} = useSelector(state => state.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const getAllProductsFilter = async () => {
       const response = await API.get(`/api/products?limit=${page*20}&${category}&${sort}&title[regex]=${search}`);
       // const response = await API.get(`/api/products?limit=${1*20}&&&title[regex]=`);
-      dispatch(getProducts(response.data.products));
+      dispatch(getProductsFilter(response.data.products));
+        dispatch(setResultFilter(response.data.result));
     };
     getAllProductsFilter();
   }, [dispatch,page,category,sort,search]);
 };
 
-export default ProductAPI;
+export default ProductFilterAPI;
