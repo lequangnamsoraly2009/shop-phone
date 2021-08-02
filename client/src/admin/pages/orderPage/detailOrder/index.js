a627dd662a41c8ffecd4eded701b5f3f1bb2fc2b
soraly <lequangnam1617@gmail.com>
2021-08-02 18:05:39 +0700

    order detail

--- client/src/admin/pages/orderPage/detailOrder/index.js
+++ client/src/admin/pages/orderPage/detailOrder/index.js
@@ -3,27 +3,41 @@ import { Breadcrumb, Button, Table } from "antd";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
-import { columnTable } from "./tableColumn";
+import {
+  columnDataBuyer,
+  columnDataReceiver,
+  columnPayment,
+  columnTable,
+} from "./tableColumn";
+import "./tableColumn";
 
 function DetailOrder() {
   const [detailOrder, setDetailOrder] = useState({});
+  const [numberOrderUser, setNumberOrderUser] = useState(0);
 
   const { payments } = useSelector((state) => state.payments);
 
   const history = useHistory();
   const params = useParams();
 
-  //   console.log(detailOrder);
+  const arrayDetail = [];
+  arrayDetail.push({ numberOrderUser, ...detailOrder });
+
+  //   console.log(numberOrderUser);
 
   useEffect(() => {
     if (params) {
+      let sumOrders = 0;
       payments.forEach((payment) => {
         if (payment._id === params.id) {
           setDetailOrder(payment);
         }
+        if (payment.user_id === detailOrder.user_id) {
+          setNumberOrderUser((sumOrders += 1));
+        }
       });
     }
-  }, [params, payments]);
+  }, [params, payments, detailOrder, numberOrderUser]);
 
   const backPreviousPage = (e) => {
     e.preventDefault();
@@ -52,12 +66,58 @@ function DetailOrder() {
           <Breadcrumb.Item>Detail Orders</Breadcrumb.Item>
         </Breadcrumb>
       </div>
-      <div className="product_data">
-        <Table
-          columns={columnTable}
-          dataSource={detailOrder.cart}
-          pagination={{ position: ["none", "none"] }}
-        />
+      <div className="container-special">
+        <div className="order_header">
+          <div className="order_header-buyer">
+            <span>Information Buyer</span>
+          </div>
+          <div className="order_header-infor-buyer">
+            <Table
+              dataSource={arrayDetail}
+              columns={columnDataBuyer}
+              style={{ marginLeft: 50 }}
+              pagination={{ position: ["none", "none"] }}
+            />
+          </div>
+        </div>
+        <div className="order_header">
+          <div className="order_header-buyer">
+            <span>Information Receiver</span>
+          </div>
+          <div className="order_header-infor-buyer">
+            <Table
+              dataSource={arrayDetail}
+              columns={columnDataReceiver}
+              style={{ marginLeft: 50 }}
+              pagination={{ position: ["none", "none"] }}
+            />
+          </div>
+        </div>
+        <div className="product_data order_header">
+          <div className="order_header-buyer">
+            <span>List Items Order</span>
+          </div>
+          <Table
+            // rowKey={(record) => record._id}
+            style={{ marginLeft: 50 }}
+            columns={columnTable}
+            dataSource={detailOrder.cart}
+            pagination={{ position: ["none", "none"] }}
+          />
+        </div>
+        <div className="order-footer order_header">
+          <div className="order_header-buyer">
+            <span>Payment</span>
+          </div>
+          <div className="order_header-infor-buyer">
+            <Table
+              style={{ marginLeft: 50 }}
+              columns={columnPayment}
+              dataSource={arrayDetail}
+              pagination={{ position: ["none", "none"] }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/admin/pages/orderPage/detailOrder/tableColumn.js
+++ client/src/admin/pages/orderPage/detailOrder/tableColumn.js
@@ -1,3 +1,4 @@
+import { Button, Space } from "antd";
 import "./tableColumn.css";
 
 export const columnTable = [
@@ -5,7 +6,7 @@ export const columnTable = [
     title: "Product Name",
     dataIndex: "name",
     key: "name",
-    width: "40%",
+    width: "30%",
     render: (text, record, index) => (
       <div className="order-table-product">
         <a href={`/admin/products/${record._id}`} className="order-table-img">
@@ -33,6 +34,7 @@ export const columnTable = [
     title: "Price",
     dataIndex: "price",
     key: "price",
+    align: "center",
     width: 200,
     render: (text, record, index) => <span>{record.price} $</span>,
   },
@@ -40,19 +42,31 @@ export const columnTable = [
     title: "Quantity",
     dataIndex: "quantity",
     key: "quantity",
+    align: "center",
     width: 150,
     render: (text, record, index) => <span>{record.quantity}</span>,
+  },
+  {
+    title: "Pre Price",
+    key: "Pre_price",
+    align: "center",
+    width: 200,
+    render: (text, record, index) => (
+      <span>{record.price * record.quantity} $</span>
+    ),
   },
   {
     title: "Sale",
     key: "sale",
     dataIndex: "sale",
+    align: "center",
     width: 200,
     render: (text, record, index) => <span>{record.sale} %</span>,
   },
   {
     title: "Provisional",
     key: "provisional",
+    align: "center",
     render: (text, record, index) =>
       record.sale === 0 ? (
         <span>{record.price * record.quantity} $</span>
@@ -65,5 +79,169 @@ export const columnTable = [
           $
         </span>
       ),
+  },
+];
+
+export const columnDataBuyer = [
+  {
+    title: "Name Customer",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record, index) => <span>{record.name}</span>,
+  },
+  {
+    title: "Email Customer",
+    dataIndex: "email",
+    key: "email",
+    align: "center",
+    render: (text, record, index) => <span>{record.email}</span>,
+  },
+  {
+    title: "Number Phone",
+    dataIndex: "phone",
+    key: "phone",
+    align: "center",
+    render: (text, record, index) => (
+      <span>{record.phone === undefined ? "Unknown" : `0${record.phone}`}</span>
+    ),
+  },
+  {
+    title: "Total Orders Placed ",
+    dataIndex: "total_orders",
+    key: "total_orders",
+    align: "center",
+    render: (text, record, index) => <span>{record.numberOrderUser}</span>,
+  },
+];
+
+export const columnDataReceiver = [
+  {
+    title: "Name Customer",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record, index) => <span>{record.name}</span>,
+  },
+  {
+    title: "Email Customer",
+    dataIndex: "email",
+    key: "email",
+    align: "center",
+    render: (text, record, index) => <span>{record.email}</span>,
+  },
+  {
+    title: "Number Phone",
+    dataIndex: "phone",
+    key: "phone",
+    align: "center",
+    render: (text, record, index) => (
+      <span>{record.phone === undefined ? "Unknown" : `0${record.phone}`}</span>
+    ),
+  },
+  {
+    title: "Payment Method",
+    dataIndex: "method",
+    key: "method",
+    align: "center",
+    render: (text, record, index) => <span>{record.numberOrderUser}</span>,
+  },
+  {
+    title: "Note",
+    dataIndex: "note",
+    width: "30%",
+    key: "note",
+    align: "center",
+    render: (text, record, index) => <span>{record.note}</span>,
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+    width: "30%",
+    align: "center",
+    render: (text, record, index) => <span>{record.address?.line1}</span>,
+  },
+];
+
+export const columnPayment = [
+  {
+    title: "Total Price",
+    dataIndex: "total_price",
+    key: "total_price",
+    render: (text, record, index) => (
+      <span>
+        {record.cart?.reduce((item1, item2) => {
+          return item1 + item2.price * item2.quantity;
+        }, 0)}
+        $
+      </span>
+    ),
+  },
+  {
+    title: "Price Sale",
+    dataIndex: "price_sale",
+    key: "price_sale",
+    align: "center",
+    render: (text, record, index) => (
+      <span>
+        {record.cart?.reduce((item1, item2) => {
+          return (
+            item1 +
+            Math.round((item2.price * item2.quantity * item2.sale) / 100)
+          );
+        }, 0)}
+        $
+      </span>
+    ),
+  },
+  {
+    title: "Transport Fee",
+    dataIndex: "fee",
+    key: "fee",
+    align: "center",
+    render: (text, record, index) => <span>{10}$</span>,
+  },
+  {
+    title: "Total Price To Pay",
+    dataIndex: "price_pay",
+    key: "price_pay",
+    align: "center",
+    render: (text, record, index) => (
+      <span>
+        {record.cart?.reduce((item1, item2) => {
+          return (
+            item1 +
+            Math.round(
+              item2.price * item2.quantity -
+                (item2.price * item2.quantity * item2.sale) / 100
+            )
+          );
+        }, 10)}
+        $
+      </span>
+    ),
+  },
+  {
+    title: "Print Invoice",
+    key: "print_invoice",
+    align: "center",
+    render: (text, record, index) => <Button type="link">Print PDF</Button>,
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    key: "status",
+    align: "center",
+    render: (text, record, index) => <span>{record.status}</span>,
+  },
+  {
+    title: "Actions",
+    key: "actions",
+    align: "center",
+    render: (text, record, index) => (
+      <Space size="middle">
+        <Button type="primary">Accept and Delivery</Button>
+        <Button type="danger">Cancel</Button>
+      </Space>
+    ),
   },
 ];
