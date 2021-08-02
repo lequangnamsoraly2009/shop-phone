99f6cfed1d8c98340a7f77b367a9c544ddb1f657
soraly <lequangnam1617@gmail.com>
2021-08-02 12:08:13 +0700

    table orders

--- client/src/admin/pages/orderPage/detailOrder/index.js
+++ client/src/admin/pages/orderPage/detailOrder/index.js
@@ -1,10 +1,29 @@
 import { ArrowLeftOutlined, HomeOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button } from "antd";
-import React from "react";
-import { useHistory } from "react-router-dom";
+import { Breadcrumb, Button, Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { columnTable } from "./tableColumn";
 
 function DetailOrder() {
+  const [detailOrder, setDetailOrder] = useState({});
+
+  const { payments } = useSelector((state) => state.payments);
+
   const history = useHistory();
+  const params = useParams();
+
+  //   console.log(detailOrder);
+
+  useEffect(() => {
+    if (params) {
+      payments.forEach((payment) => {
+        if (payment._id === params.id) {
+          setDetailOrder(payment);
+        }
+      });
+    }
+  }, [params, payments]);
 
   const backPreviousPage = (e) => {
     e.preventDefault();
@@ -14,7 +33,13 @@ function DetailOrder() {
     <div className="container-admin">
       <div className="header_page">
         <h3>
-          <Button style={{marginRight: 10}} type="dashed" icon={<ArrowLeftOutlined />} onClick={backPreviousPage} />Orders From Customers
+          <Button
+            style={{ marginRight: 10 }}
+            type="dashed"
+            icon={<ArrowLeftOutlined />}
+            onClick={backPreviousPage}
+          />
+          Orders From Customers
         </h3>
       </div>
       <div className="product_breadcrumb">
@@ -27,7 +52,13 @@ function DetailOrder() {
           <Breadcrumb.Item>Detail Orders</Breadcrumb.Item>
         </Breadcrumb>
       </div>
-      <div className="product_data"></div>
+      <div className="product_data">
+        <Table
+          columns={columnTable}
+          dataSource={detailOrder.cart}
+          pagination={{ position: ["none", "none"] }}
+        />
+      </div>
     </div>
   );
 }
---
+++ client/src/admin/pages/orderPage/detailOrder/tableColumn.js
@@ -0,0 +1,69 @@
+import "./tableColumn.css";
+
+export const columnTable = [
+  {
+    title: "Product Name",
+    dataIndex: "name",
+    key: "name",
+    width: "40%",
+    render: (text, record, index) => (
+      <div className="order-table-product">
+        <a href={`/admin/products/${record._id}`} className="order-table-img">
+          <img
+            src={record.images.url}
+            alt={record.images.public_id}
+            style={{ height: 100, width: 100, objectFit: "scale-down" }}
+          />
+        </a>
+        <div className="order-table-info">
+          <a href={`/admin/products/${record._id}`}>
+            <span style={{ fontSize: 15, fontWeight: 600 }}>
+              {record.title.toUpperCase()}
+            </span>
+          </a>
+          <div style={{ fontSize: 12 }}>
+            <span>Product ID: </span>
+            <span>{record.product_id}</span>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Price",
+    dataIndex: "price",
+    key: "price",
+    width: 200,
+    render: (text, record, index) => <span>{record.price} $</span>,
+  },
+  {
+    title: "Quantity",
+    dataIndex: "quantity",
+    key: "quantity",
+    width: 150,
+    render: (text, record, index) => <span>{record.quantity}</span>,
+  },
+  {
+    title: "Sale",
+    key: "sale",
+    dataIndex: "sale",
+    width: 200,
+    render: (text, record, index) => <span>{record.sale} %</span>,
+  },
+  {
+    title: "Provisional",
+    key: "provisional",
+    render: (text, record, index) =>
+      record.sale === 0 ? (
+        <span>{record.price * record.quantity} $</span>
+      ) : (
+        <span>
+          {Math.floor(
+            record.price * record.quantity -
+              (record.price * record.quantity * record.sale) / 100
+          )}{" "}
+          $
+        </span>
+      ),
+  },
+];
