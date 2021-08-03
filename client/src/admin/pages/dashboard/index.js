480dd254e6947f35cf164808ae55c10dbff4bdfb
soraly <lequangnam1617@gmail.com>
2021-08-03 23:57:05 +0700

    filter

--- client/src/admin/pages/dashboard/index.js
+++ client/src/admin/pages/dashboard/index.js
@@ -9,9 +9,25 @@ import CardTotal from "./components/cardTotal";
 
 function DashBoard() {
   const { deviceUsers } = useSelector((state) => state.usersAdmin);
+  const { payments } = useSelector((state) => state.payments);
   const [operatingSystem, setOperatingSystem] = useState([]);
   const [client, setClient] = useState([]);
   const [device, setDevice] = useState([]);
+
+
+  const totalPrice = payments.reduce((payment1, payment2) => {
+    return (
+      payment1 +
+      payment2.cart.reduce((item1, item2) => {
+        return (
+          item1 +
+          (item2.price * item2.quantity -
+            Math.round((item2.price * item2.quantity * item2.sale) / 100))
+        );
+      }, 0)
+    );
+  }, 0);
+  console.log(totalPrice);
 
   useEffect(() => {
     let oSys = [];
@@ -184,9 +200,7 @@ function DashBoard() {
         <div className="chart-card-list">
           <CardTotal />
         </div>
-        <div>
-          
-        </div>
+        <div></div>
       </div>
     </Col>
   );
