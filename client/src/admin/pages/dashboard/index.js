9401ab475c4f71ef687aeecb3a1d14f05eb57074
soraly <lequangnam1617@gmail.com>
2021-08-03 17:13:28 +0700

    chart pie operating system

--- client/src/admin/pages/dashboard/index.js
+++ client/src/admin/pages/dashboard/index.js
@@ -1,13 +1,73 @@
 import { Breadcrumb, Col } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../page.css";
 import "./dashboard.css";
 import { HomeOutlined } from "@ant-design/icons";
 import { useSelector } from "react-redux";
+import { Pie } from "react-chartjs-2";
 
 function DashBoard() {
   const { deviceUsers } = useSelector((state) => state.usersAdmin);
-  console.log(deviceUsers);
+  const [operatingSystem, setOperatingSystem] = useState([]);
+  const [client, setClient] = useState([]);
+  const [device, setDevice] = useState([]);
+
+  useEffect(() => {
+    let oSys = [];
+    let cli = [];
+    let device = [];
+    deviceUsers.forEach((user) => {
+      oSys.push(user.resultDevice?.os);
+      cli.push(user.resultDevice?.client);
+      device.push(user.resultDevice?.device);
+    });
+    setOperatingSystem(oSys);
+    setClient(cli);
+    setDevice(device);
+  }, [deviceUsers]);
+
+  console.log({ operatingSystem: operatingSystem });
+  console.log({ client: client });
+  console.log({ device: device });
+
+  const dataOs = () => {
+    let and = 0;
+    let ios = 0;
+    let linux = 0;
+    let win = 0;
+    let mac = 0;
+    let diff = 0;
+    operatingSystem.forEach((item) => {
+      if (item?.name === "Mac") {
+        mac += 1;
+      } else if (item?.name === "Windows") {
+        win += 1;
+      } else if (item?.name === "Android") {
+        and += 1;
+      } else if (item?.name === "iOS") {
+        ios += 1;
+      } else if (item?.name === "Linux") {
+        linux += 1;
+      } else {
+        diff += 1;
+      }
+    });
+    return [and, ios, win, mac, linux, diff];
+  };
+
+  console.log(dataOs());
+
+  const dataPie = {
+    labels: ["Android", "iOS", "Windows", "MacOs", "Linux", "Different"],
+    datasets: [
+      {
+        label: "Dataset OS (%)",
+        data: dataOs(),
+        backgroundColor: ["red", "green", "purple", "blue", "black", "yellow"],
+      },
+    ],
+  };
+
   return (
     <Col className="gutter-row" span={21}>
       <div>
@@ -21,6 +81,14 @@ function DashBoard() {
             </Breadcrumb.Item>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
           </Breadcrumb>
+        </div>
+        <div>
+          <Pie
+            data={dataPie}
+            width={300}
+            height={300}
+            options={{ maintainAspectRatio: false }}
+          />
         </div>
       </div>
     </Col>
