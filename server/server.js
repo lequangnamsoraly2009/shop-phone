a56e6dc99045fe57b8f73e91d53dfd97df6b3f88
soraly <lequangnam1617@gmail.com>
2021-08-05 18:09:58 +0700

    add extra cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true,
+}));
 app.use(
   fileUpload({
     useTempFiles: true,
