3c2632f84773a5c0dce6598fe63ba968885b2791
soraly <lequangnam1617@gmail.com>
2021-08-07 16:16:02 +0700

    delete VNpay

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,10 +1,6 @@
 const Payments = require("../models/payment.model");
 const Users = require("../models/user.model");
 const Products = require("../models/product.model");
-const config = require("config");
-const dateFormat = require("dateformat");
-const querystring = require("qs");
-const sha256 = require("sha256");
 
 class APIfeatures {
   constructor(query, queryString) {
@@ -137,117 +133,6 @@ const paymentController = {
       return res.status(500).json({ status: false, message: error.message });
     }
   },
-  createPaymentByVNPay: async (req, res) => {
-    const ipAddr =
-      req.headers["x-forwarded-for"] ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
-
-    const tmnCode = config.get("vnp_TmnCode");
-    const secretKey = config.get("vnp_HashSecret");
-    let vnpUrl = config.get("vnp_Url");
-    const returnUrl = config.get("vnp_ReturnUrl");
-
-    const date = new Date();
-
-    const createDate = dateFormat(date, "yyyymmddHHmmss");
-    const orderId = dateFormat(date, "HHmmss");
-    const amount = req.body.amount;
-    const bankCode = req.body.bankCode;
-
-    const orderInfo = req.body.orderDescription;
-    const orderType = req.body.orderType;
-    const locale = req.body.language;
-    if (locale === null || locale === "") {
-      locale = "vn";
-    }
-
-    const currCode = "VND";
-    let vnp_Params = {};
-    vnp_Params["vnp_Version"] = "2";
-    vnp_Params["vnp_Command"] = "pay";
-    vnp_Params["vnp_TmnCode"] = tmnCode;
-    // vnp_Params['vnp_Merchant'] = ''
-    vnp_Params["vnp_Locale"] = locale;
-    vnp_Params["vnp_CurrCode"] = currCode;
-    vnp_Params["vnp_TxnRef"] = orderId;
-    vnp_Params["vnp_OrderInfo"] = orderInfo;
-    vnp_Params["vnp_OrderType"] = orderType;
-    vnp_Params["vnp_Amount"] = amount * 100;
-    vnp_Params["vnp_ReturnUrl"] = returnUrl;
-    vnp_Params["vnp_IpAddr"] = ipAddr;
-    vnp_Params["vnp_CreateDate"] = createDate;
-    if (bankCode !== null && bankCode !== "") {
-      vnp_Params["vnp_BankCode"] = bankCode;
-    }
-
-    vnp_Params = sortObject(vnp_Params);
-
-    const signData =
-      secretKey + querystring.stringify(vnp_Params, { encode: false });
-
-    const secureHash = sha256(signData);
-
-    vnp_Params["vnp_SecureHashType"] = "SHA256";
-    vnp_Params["vnp_SecureHash"] = secureHash;
-    vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: true });
-
-    //Neu muon dung Redirect thi dong dong ben duoi
-    // res.status(200).json({ code: "00", data: vnpUrl });
-    //Neu muon dung Redirect thi mo dong ben duoi va dong dong ben tren
-    res.json({
-      vnpUrl:vnpUrl
-    })
-  },
-  dataPaymentByVNPay: async (req, res) => {
-    let vnp_Params = req.query;
-    let secureHash = vnp_Params["vnp_SecureHash"];
-
-    delete vnp_Params["vnp_SecureHash"];
-    delete vnp_Params["vnp_SecureHashType"];
-
-    vnp_Params = sortObject(vnp_Params);
-    let secretKey = config.get("vnp_HashSecret");
-    let signData =
-      secretKey + querystring.stringify(vnp_Params, { encode: false });
-
-    let checkSum = sha256(signData);
-
-    if (secureHash === checkSum) {
-      let orderId = vnp_Params["vnp_TxnRef"];
-      let rspCode = vnp_Params["vnp_ResponseCode"];
-      res.json({ orderId, rspCode });
-      //Kiem tra du lieu co hop le khong, cap nhat trang thai don hang va gui ket qua cho VNPAY theo dinh dang duoi
-      res.status(200).json({ RspCode: "00", Message: "success" });
-    } else {
-      res.status(200).json({ RspCode: "97", Message: "Fail checksum" });
-    }
-  },
-  returnPaymentByVNPay: async (req, res) => {
-    let vnp_Params = req.query;
-
-    let secureHash = vnp_Params["vnp_SecureHash"];
-
-    delete vnp_Params["vnp_SecureHash"];
-    delete vnp_Params["vnp_SecureHashType"];
-
-    vnp_Params = sortObject(vnp_Params);
-
-    let tmnCode = config.get("vnp_TmnCode");
-    let secretKey = config.get("vnp_HashSecret");
-
-    let signData =
-      secretKey + querystring.stringify(vnp_Params, { encode: false });
-
-    let checkSum = sha256(signData);
-
-    if (secureHash === checkSum) {
-      //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
-
-      res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
-    }
-  },
 };
 
 const countSoldAndStorage = async (id, quantity, oldSold, oldStorage) => {
@@ -260,23 +145,5 @@ const countSoldAndStorage = async (id, quantity, oldSold, oldStorage) => {
   );
 };
 
-function sortObject(o) {
-  var sorted = {},
-    key,
-    a = [];
-
-  for (key in o) {
-    if (o.hasOwnProperty(key)) {
-      a.push(key);
-    }
-  }
-
-  a.sort();
-
-  for (key = 0; key < a.length; key++) {
-    sorted[a[key]] = o[a[key]];
-  }
-  return sorted;
-}
 
 module.exports = paymentController;
