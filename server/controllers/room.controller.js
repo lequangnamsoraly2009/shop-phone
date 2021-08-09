8428280852f4ebb32154962018491076349cab99
soraly <lequangnam1617@gmail.com>
2021-08-09 13:42:07 +0700

    create room for user when register account

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -1,13 +1,37 @@
-const Room = require("../models/room.model");
+const Rooms = require("../models/room.model");
+const Users = require("../models/user.model");
 
 const roomController = {
   getRooms: async (req, res) => {
     try {
-      const rooms = await Room.find();
+      const rooms = await Rooms.find();
       res.json({ status: "success", results: rooms.length, rooms: rooms });
     } catch (error) {
       return res.status(500).json({ status: false, message: error.message });
     }
+  },
+  getRoomByUserID: async (req, res) => {
+    try {
+      const { userId } = req.body;
+
+      const user = await Users.findById({ _id: userId });
+
+      if (!user)
+        return res
+          .status(404)
+          .json({ status: false, message: "User not found" });
+
+      const roomOfUser = await Room.findOne({ userId: userId });
+
+      res.json({ status: "success", roomOfUser: roomOfUser });
+    } catch (error) {
+      return res.status(500).json({ status: false, message: error.message });
+    }
+  },
+  //   Tạo room cho user ngay sau khi đăng kí bằng account local hoặc đăng kí bằng google account
+  createRoom: async (req, res) => {
+    try {
+    } catch (error) {}
   },
 };
 
