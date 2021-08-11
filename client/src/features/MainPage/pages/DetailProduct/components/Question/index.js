2a579c6caea40030fe4f40bade5da97384268898
soraly <lequangnam1617@gmail.com>
2021-08-11 21:12:35 +0700

    Q&A left

--- client/src/features/MainPage/pages/DetailProduct/components/Question/index.js
+++ client/src/features/MainPage/pages/DetailProduct/components/Question/index.js
@@ -1,14 +1,55 @@
-import React from 'react'
-import "./question.css"
+import React from "react";
+import "./question.css";
 
 function QuestionAndAnswers() {
-    return (
-        <div className="question">
-            <div className="question-left">
-            </div>
-            <div className="question-right"></div>
+  return (
+    <div className="question">
+      <div className="question-left">
+        <div className="question_left-header">
+          <span>There are a total of 100 questions</span>
         </div>
-    )
+        <div
+          className="btn"
+          style={{
+            width: "100%",
+            textAlign: "center",
+            backgroundColor: "rgb(201,232,242)",
+            cursor: "pointer",
+            color: "red",
+            letterSpacing: "1px",
+          }}
+          //   onClick={addCartItem}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Ask a Question
+        </div>
+        <div
+          className="btn"
+          style={{
+            marginTop: 10,
+            width: "100%",
+            textAlign: "center",
+            backgroundColor: "rgb(201,232,242)",
+            cursor: "pointer",
+            color: "red",
+            letterSpacing: "1px",
+          }}
+          //   onClick={addCartItem}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          See All Questions
+        </div>
+      </div>
+
+      <div className="question-right"></div>
+    </div>
+  );
 }
 
-export default QuestionAndAnswers
+export default QuestionAndAnswers;
