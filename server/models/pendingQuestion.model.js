41c7097e7c6c4865ee207a45c56b1d9a5b6d8e4a
soraly <lequangnam1617@gmail.com>
2021-08-16 21:08:34 +0700

    create model pending confirm question

---
+++ server/models/pendingQuestion.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const pendingQuestionProductSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    question: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "uncomfirmed",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("PendingQuestionProducts", pendingQuestionProductSchema);
