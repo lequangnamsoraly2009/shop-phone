a236225cabd6a78495d31cb3f8ea428f428b9b17
soraly <lequangnam1617@gmail.com>
2021-08-19 23:20:31 +0700

    UPDATE: succes add more information for product

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -79,12 +79,66 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    keySpecs: {
+    display: {
       type: Object,
       default: {
-        operatingSystem: "",
-        internalMemory: 0,
-        
+        screenTechnology: "IPS LCD",
+        resolution: "HD+ (720 x 1560 Pixels)",
+        wideScreen: "6.39 - Tần số quét 60 Hz",
+        maximumBrightness: "400 nits",
+      },
+    },
+    rareCamera: {
+      type: Object,
+      default: {
+        resolution: "64 MP",
+        flash: "Yes",
+      },
+    },
+    frontCamera: {
+      type: Object,
+      default: {
+        resolution: "16 MP",
+      },
+    },
+    cpu: {
+      type: Object,
+      default: {
+        os: "Android 10",
+        chip: "Snapdragon 460 8 cores",
+        cpuSpeed: "1.8 GHz",
+        gpu: "Adreno 610",
+      },
+    },
+    memoryAndStorage: {
+      type: Object,
+      default: {
+        ram: "4 GB",
+        internalMemory: "64 GB",
+        memoryStick: "Max 512Gb",
+      },
+    },
+    connect: {
+      type: Object,
+      default: {
+        bluetooth: "v4.2",
+        chargingInterface: "Type-C",
+        headphoneJack: "3.5 mm",
+      },
+    },
+    batteries: {
+      type: Object,
+      default: {
+        batteryCapacity: "4000 mAh",
+        batteryType: "Li-Ion",
+      },
+    },
+    general: {
+      type: Object,
+      default: {
+        weight: "180g",
+        releaseTime: "12/2020",
+        design: "Monolithic",
       },
     },
     subImages: {
