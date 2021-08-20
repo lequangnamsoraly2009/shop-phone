f15da793a965836d7dd1910d9dc016ee5c1ede99
soraly <lequangnam1617@gmail.com>
2021-08-20 15:01:00 +0700

    UPDATE: create product

--- client/src/admin/pages/productPage/pages/createProduct/index.js
+++ client/src/admin/pages/productPage/pages/createProduct/index.js
@@ -41,8 +41,8 @@ function CreateProduct() {
   };
 
   useEffect(() => {
-   dispatch(getAllCategories(""));
-  },[dispatch])
+    dispatch(getAllCategories(""));
+  }, [dispatch]);
 
   const onFinishForm = async (values) => {
     try {
@@ -53,13 +53,17 @@ function CreateProduct() {
         }
       });
       if (onEdit) {
-        let images = { ...image};
+        let images = { ...image };
         const product = { ...values, nameCategory: nameCate, images: images };
-        await ProductFilterAPI.editProduct({paramID:param.id, product, token})
+        await ProductFilterAPI.editProduct({
+          paramID: param.id,
+          product,
+          token,
+        });
       } else {
         let images = { ...image?.response };
         const product = { ...values, nameCategory: nameCate, images: images };
-        await ProductFilterAPI.createProduct({product,token})
+        await ProductFilterAPI.createProduct({ product, token });
       }
       setImage({});
       history.push("/admin/products");
@@ -115,7 +119,13 @@ function CreateProduct() {
     <div className="container-admin">
       <div className="header_page">
         <h3>
-          <Button style={{marginRight: 10}} type="dashed" icon={<ArrowLeftOutlined />} onClick={backPreviousPage} />Create New Product
+          <Button
+            style={{ marginRight: 10 }}
+            type="dashed"
+            icon={<ArrowLeftOutlined />}
+            onClick={backPreviousPage}
+          />
+          Create New Product
         </h3>
       </div>
       <div className="product_breadcrumb">
@@ -125,7 +135,9 @@ function CreateProduct() {
           </Breadcrumb.Item>
           <Breadcrumb.Item href="/home">Home</Breadcrumb.Item>
           <Breadcrumb.Item href="/admin/products">Products</Breadcrumb.Item>
-          <Breadcrumb.Item>{!onEdit ? "Create" : "Update"} Product</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            {!onEdit ? "Create" : "Update"} Product
+          </Breadcrumb.Item>
         </Breadcrumb>
       </div>
       <div className="create_header">
@@ -218,7 +230,7 @@ function CreateProduct() {
                 },
               ]}
             >
-              <Select placeholder="Please select a category">
+              <Select showSearch placeholder="Please select a category">
                 {categories.map((category) => (
                   <Option key={category._id} value={category._id}>
                     {category.nameCategory}
@@ -227,7 +239,7 @@ function CreateProduct() {
               </Select>
             </Form.Item>
             <Form.Item label="Status" name="status" hasFeedback>
-              <Select placeholder="Default: Stocking">
+              <Select  showSearch placeholder="Default: Stocking">
                 <Option value="Stocking">Stocking</Option>
                 <Option value="OutStocking">OutStocking</Option>
                 <Option value="Importing">Importing</Option>
@@ -241,7 +253,7 @@ function CreateProduct() {
                 { required: true, message: "Please select color for product!" },
               ]}
             >
-              <Select placeholder="Please select color for product">
+              <Select showSearch placeholder="Please select color for product">
                 <Option value="yellow">Yellow</Option>
                 <Option value="gold">Gold</Option>
                 <Option value="red">Red</Option>
@@ -253,7 +265,77 @@ function CreateProduct() {
                 <Option value="silver">Silver</Option>
               </Select>
             </Form.Item>
+            <h3 style={{color: "rgb(25,144,255)"}}>Display Product:</h3>
             <Form.Item
+              label="Screen Technology"
+              name="screenTechnology"
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Please select Screen Technology for product!",
+                },
+              ]}
+            >
+              <Select showSearch placeholder="Please select Screen Technology for product">
+                <Option value="LCD">LCD</Option>
+                <Option value="TFT - LCD">TFT - LCD</Option>
+                <Option value="Super LCD">Super LCD</Option>
+                <Option value="IPS LCD">IPS LCD</Option>
+                <Option value="LED-backlit IPS LCD">LED-backlit IPS LCD</Option>
+                <Option value="LTPS LCD">LTPS LCD</Option>
+                <Option value="IPS Quantum">IPS Quantum</Option>
+                <Option value="OLED">OLED</Option>
+                <Option value="AMOLED">AMOLED</Option>
+                <Option value="Super AMOLED">Super AMOLED</Option>
+                <Option value="Super AMOLED Plus">Super AMOLED Plus</Option>
+                <Option value="Super AMOLED HD">Super AMOLED HD</Option>
+                <Option value="P-OLED">P-OLED</Option>
+                <Option value="ClearBlack">ClearBlack</Option>
+                <Option value="NOVA">NOVA</Option>
+                <Option value="Mobile BRAVIA Engine">
+                  Mobile BRAVIA Engine
+                </Option>
+                <Option value="Retina">Retina</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Resolution"
+              name="resolution"
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Please select resolution for product!",
+                },
+              ]}
+            >
+              <Select showSearch placeholder="Please select resolution for product">
+                <Option value="QQVGA (120 x 160 pixels)">QQVGA (120 x 160 pixels)</Option>
+                <Option value="QVGA (320 x 240 pixels)">QVGA (320 x 240 pixels)</Option>
+                <Option value="WQVGA (428 x 240 pixels)">WQVGA (428 x 240 pixels)</Option>
+                <Option value="VGA (640 x 480 pixels)">VGA (640 x 480 pixels)</Option>
+                <Option value="WVGA (768 x 480 pixels)">WVGA (768 x 480 pixels)</Option>
+                <Option value="FWVGA (854 x 480 pixels)">FWVGA (854 x 480 pixels)</Option>
+                <Option value="SVGA (800 x 600 pixels)">SVGA (800 x 600 pixels)</Option>
+                <Option value="DVGA (960 x 640 pixels)">DVGA (960 x 640 pixels)</Option>
+                <Option value="qHD (960 x 540 pixels)">qHD (960 x 540 pixels)</Option>
+                <Option value="HD (1.280 × 720 pixels)">HD (1.280 × 720 pixels)</Option>
+                <Option value="HD+ (1.440 x 720 pixels)">HD+  ( 1.440 x 720 pixels)</Option>
+                <Option value="XGA (1.024 x 768 pixels)">XGA (1.024 x 768 pixels)</Option>
+                <Option value="WXGA (1.366 x 768 pixels)">WXGA (1.366 x 768 pixels)</Option>
+                <Option value="QVGA (320 x 240 pixels)">QVGA (320 x 240 pixels)</Option>
+                <Option value="Full HD (1.920 × 1.080 pixels)">Full HD (1.920 × 1.080 pixels)</Option>
+                <Option value="FHD+ (2.160 x 1.080 pixels)">FHD+ (2.160 x 1.080 pixels)</Option>
+                <Option value="2K (2.560 x 1.440 pixels)">2K (2.560 x 1.440 pixels)</Option>
+                <Option value="2K+ (2.960 x 1.440 pixelss)">2K+ (2.960 x 1.440 pixels)</Option>
+                <Option value="4K (3.840 x 2.160 pixels)">4K (3.840 x 2.160 pixels)</Option>
+                <Option value="Ultra HD (5.120 x 2.880 pixels)">Ultra HD (5.120 x 2.880 pixels)</Option>
+
+              </Select>
+            </Form.Item>
+
+            {/* <Form.Item
               label="Memory"
               name="memory"
               hasFeedback
@@ -273,7 +355,7 @@ function CreateProduct() {
                 <Option value="512">512</Option>
                 <Option value="1024">1024</Option>
               </Select>
-            </Form.Item>
+            </Form.Item> */}
             <Form.Item
               label="Price"
               name="price"
