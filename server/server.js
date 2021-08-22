90c168620c4aaaff5a11b93ca7085426f85255c0
soraly <lequangnam1617@gmail.com>
2021-08-22 16:01:28 +0700

    FIX error: request entity too large

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const db = require("./database/config.db");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
+
+// app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
