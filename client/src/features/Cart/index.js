06ea443d1e85ce3384287ec61dae5b929a9c6ec5
soraly <lequangnam1617@gmail.com>
2021-08-23 20:27:01 +0700

    config cart payment

--- client/src/features/Cart/index.js
+++ client/src/features/Cart/index.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Table, Tag, Space, Breadcrumb, Input } from "antd";
+import {
+  Table,
+  Tag,
+  Space,
+  Breadcrumb,
+  Input,
+  Form,
+  InputNumber,
+  Button,
+} from "antd";
 import CartBanner from "./components/CartBanner";
 import CartEmpty from "./components/CartEmpty";
 import "./cart.css";
@@ -13,7 +22,12 @@ import {
 import API from "../../api/axiosClient";
 import Swal from "sweetalert2";
 
-const {Search} = Input;
+const { Search } = Input;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 
 function Cart() {
   const { carts, isLoadingCart } = useSelector((state) => state.carts);
@@ -174,6 +188,10 @@ function Cart() {
     },
   };
 
+  const onFinishInformation = (values) => {
+    console.log(values);
+  };
+
   const sendPayMentCart = () => {
     if (isLoggedIn === false) {
       Swal.fire({
@@ -227,6 +245,25 @@ function Cart() {
               />
             </div>
           </div>
+          <div className="cart-total-wrapper">
+            <div className="cart-total-price">
+              <div className="cart-total">
+                <h2 style={{ fontWeight: 800 }}>Total Cart</h2>
+              </div>
+              <div className="cart-total  ">
+                <span>Totals Price ({productChoice} products):</span>
+                <span style={{ color: "rgb(25,144,255)" }}>{total} $</span>
+              </div>
+              <div className="cart-total ">
+                <span>Shipping:</span>
+                <span>0 $</span>
+              </div>
+              <div className="cart-total ">
+                <span>Total:</span>
+                <span style={{ color: "rgb(247,69,46)" }}>{total} $</span>
+              </div>
+            </div>
+          </div>
           <div className="cart-checkout">
             <div className="cart-checkout-coupon">
               <div className="cart-checkout-coupon-header">
@@ -243,77 +280,65 @@ function Cart() {
               </div>
             </div>
             <div className="cart-checkout-information">
-            <div className="cart-checkout-coupon-header">
+              <div className="cart-checkout-coupon-header">
                 <span>Receiver's Information</span>
               </div>
-            </div>
-          </div>
-          <div className="cart-total-wrapper">
-            <div className="cart-total-price">
-              <div className="cart-total">
-                <h2 style={{ fontWeight: 800 }}>Total Cart</h2>
-              </div>
-              <div className="cart-total  ">
-                <span>Totals Price ({productChoice} products):</span>
-                <span style={{ color: "rgb(25,144,255)" }}>{total} $</span>
-              </div>
-              <div className="cart-total ">
-                <span>Shipping (Default):</span>
-                <span>0 $</span>
-              </div>
-              <div className="cart-total ">
-                <span>Total:</span>
-                <span style={{ color: "rgb(247,69,46)" }}>{total} $</span>
-              </div>
-              <div className="cart-total ">
-                <div style={{ margin: "20px auto 0px auto" }}>
-                  {isLoggedIn === false ? (
-                    <>
-                      <Link
-                        className="btn"
-                        style={{ backgroundColor: "rgb(230,246,255)" }}
-                        to="/home/cart"
-                        onClick={() => sendPayMentCart()}
-                      >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        PayMent
-                      </Link>
-                    </>
-                  ) : productChoice === 0 ? (
-                    <>
-                      <Link
-                        className="btn"
-                        style={{ backgroundColor: "rgb(230,246,255)" }}
-                        to="/home/cart"
-                        onClick={() => sendPayMentCart()}
-                      >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        PayMent
-                      </Link>
-                    </>
-                  ) : (
-                    <>
-                      <Link
-                        className="btn"
-                        style={{ backgroundColor: "rgb(230,246,255)" }}
-                        to="/home/checkout"
-                        onClick={() => sendPayMentCart()}
-                      >
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        PayMent
-                      </Link>
-                    </>
-                  )}
-                </div>
+              <div className="cart-checkout-information-form">
+                <Form
+                  style={{ margin: "30px 0" }}
+                  {...layout}
+                  name="nest-messages"
+                  onFinish={onFinishInformation}
+                >
+                  <Form.Item
+                    name="nameReceiver"
+                    label="Full Name"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your full name!",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    name="email"
+                    label="Email"
+                    rules={[
+                      {
+                        type: "email",
+                        required: true,
+                        message: "Please input your email",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    name="numberPhone"
+                    label="Number Phone"
+                    rules={[
+                      {
+                        type: "number",
+                        minLength: 9,
+                        maxLength: 9,
+                        required: true,
+                      },
+                    ]}
+                  >
+                    <InputNumber style={{ width: "100%" }} />
+                  </Form.Item>
+
+                  <Form.Item name="note" label="Note">
+                    <Input.TextArea autoSize={{ minRows: 7, maxRows: 7 }} />
+                  </Form.Item>
+                  <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 15 }}>
+                    <Button type="primary" htmlType="submit">
+                      Pay
+                    </Button>
+                  </Form.Item>
+                </Form>
               </div>
             </div>
           </div>
