0e665a9ff10fb54871f2df7911f7622a806d655f
soraly <lequangnam1617@gmail.com>
2021-08-29 07:53:15 +0700

    Add field vouchersSave for user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -64,7 +64,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 1
     },
-    
+    vouchersSave:{
+      type: Array,
+      default: []
+    },
   },
   {
     timestamps: true,
